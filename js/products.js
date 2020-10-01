b7c7edf218ed6c1abb9b7a575a1010fe29903e34
Allen Obayagbona <iykeoba@gmail.com>
2020-09-30 23:11:55 -0400

    updates

--- js/products.js
+++ js/products.js
@@ -5,107 +5,76 @@ import { createOrder } from "./graphql/mutations";
 import { listProducts } from "./graphql/queries";
 
 Amplify.configure(awsconfig);
-document.addEventListener('DOMContentLoaded', () => {
-    async function createNewOrder() {
-      const order = {
-        userId: "value1", 
-        order: {
-          products: {
-            productId: "df123", 
-            quantity: 30
-          }
-        }
-      };
-    
-      return await API.graphql(graphqlOperation(createOrder, { input: order }));
+async function createNewOrder() {
+  const order = {
+    userId: "value1", 
+    order: {
+      products: {
+        productId: "df123", 
+        quantity: 30
+      }
     }
+  };
 
-    async function listAllProducts() {
-      return await API.graphql(graphqlOperation(listProducts));
+  return await API.graphql(graphqlOperation(createOrder, { input: order }));
+}
+
+async function listAllProducts() {
+  return await API.graphql(graphqlOperation(listProducts));
+}
+
+// const MutationButton = document.getElementById("MutationEventButton");
+// const MutationResult = document.getElementById("MutationResult");
+
+// MutationButton.addEventListener("click", (evt) => {
+//   createNewOrder().then((evt) => {
+//     MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+//   });
+// });
+const xprods = listAllProducts()
+loadproducts();
+
+// Load products.
+function loadproducts() {
+    const response = JSON.parse(xprods);
+    const data = response["data"][""]
+    for (i = 0; i < data.length; i++) {
+        product_result = data[i]
+        
+        if (product_result["QtyinStock"] > 0) {
+          num = product_result["QtyinStock"];
+        } else {
+          num = null;
+        }
+      
+        productjson = {
+            "name": product_result["Productname"],
+            "longname": product_result["Productname"],
+            "price": product_result["Price"],
+            "image": "images/"+product_result["productid"]+".png",
+            "id": product_result["productid"],
+            "inventory": num
+        }
+        InsertProducts(productjson)
     }
-    
-    // const MutationButton = document.getElementById("MutationEventButton");
-    // const MutationResult = document.getElementById("MutationResult");
-    
-    // MutationButton.addEventListener("click", (evt) => {
-    //   createNewOrder().then((evt) => {
-    //     MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
-    //   });
-    // });
-    const xprods = listAllProducts()
-    loadproducts();
-    getdates();
-    // Load products.
-    function loadproducts() {
-        const response = JSON.parse(xprods);
-        const data = response["data"][""]
-        for (i = 0; i < data.length; i++) {
-            product_result = data[i]
-            
-            if (product_result["QtyinStock"] > 0) {
-              num = product_result["QtyinStock"];
-            } else {
-              num = null;
-            }
+    request.send();
+};
+
           
-            productjson = {
-                "name": product_result["Productname"],
-                "longname": product_result["Productname"],
-                "price": product_result["Price"],
-                "image": "images/"+product_result["productid"]+".png",
-                "id": product_result["productid"],
-                "inventory": num
-            }
-            InsertProducts(productjson)
-        }
-        request.send();
-    };
-    // function loadproducts_() {
-    //     url = 'https://xfyws8r1e6.execute-api.us-east-1.amazonaws.com/prod/listItems'
-    //     var products = []
-    //     const request = new XMLHttpRequest();
-    //     request.open('GET', url);
-    //     request.onload = () => {
-    //         var data1 = JSON.parse(request.responseText);
-    //         var data2 = JSON.stringify(eval("(" + data1["items"] + ")"));
-    //         const data = JSON.parse(data2);
-    //         for (i = 0; i < 10; i++) {
-    //             product_result = data[i]
-                
-    //             if (product_result["numInStock"] > 0) {
-    //               num = product_result["numInStock"];
-    //             } else {
-    //               num = null;
-    //             }
-               
-    //             productjson = {
-    //                 "name": product_result["itemName"],
-    //                 "longname": product_result["itemLongName"],
-    //                 "price": product_result["price"],
-    //                 "image": "images/"+product_result["GoodId"]+".png",
-    //                 "id": product_result["GoodId"],
-    //                 "inventory": num
-    //             }
-    //             InsertProducts(productjson)
-    //         }
-    //     };
-    //     request.send();
-    // };
-              
-    function InsertProducts(postdata) {
-        // Template for products
-        const template = Handlebars.compile(document.querySelector('#load_products').innerHTML);
-        title = postdata["name"];
-        description = postdata["longname"];
-        price = postdata["price"];
-        id = postdata["id"];
-        image = postdata["image"];
-        inventory = postdata["inventory"];
-        // Insert products
-        // Add post result to DOM.
+function InsertProducts(postdata) {
+    // Template for products
+    const template = Handlebars.compile(document.querySelector('#load_products').innerHTML);
+    title = postdata["name"];
+    description = postdata["longname"];
+    price = postdata["price"];
+    id = postdata["id"];
+    image = postdata["image"];
+    inventory = postdata["inventory"];
+    // Insert products
+    // Add post result to DOM.
 
-        const product = template({'title': title, "description": description, 'price': price, 'id': id, 'image': image, 'inventory': inventory});
-        document.querySelector('#products').innerHTML += product;
-    };
+    const product = template({'title': title, "description": description, 'price': price, 'id': id, 'image': image, 'inventory': inventory});
+    document.querySelector('#products').innerHTML += product;
+};
 
-})
+
